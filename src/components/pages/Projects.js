96166337609970bcdd5aaba3b364e9946216154a
reirysson <reiryssonjp@gmail.com>
2022-05-19 17:08:50 -0300

    adicionando o resgate dos dados via API

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,36 @@
 import { useLocation } from 'react-router-dom'
 
+import { useState, useEffect } from 'react'
+
 import Message from '../layout/Message'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 import styles from './Projects.module.css'
 
 function Projects() {
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
 
     return(
         <div className={styles.project_container}>
@@ -20,7 +40,14 @@ function Projects() {
             </div>
             {message && <Message msg={message} type="success" />}
             <Container customClass="start">
-                <p>Projetos</p>
+                {projects.length > 0 &&
+                    projects.map((project) => <ProjectCard 
+                    id={project.id}
+                    name={project.name}
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                    />)}
             </Container>
         </div>
     )
